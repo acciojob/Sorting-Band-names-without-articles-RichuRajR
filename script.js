61f69f26c59f99dc0b13ec08ab366cff24b975bb
RichuRajR <118427343+RichuRajR@users.noreply.github.com>
2023-04-16 16:06:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,23 +1,14 @@
 //your code here
-// script.js
-const bands = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
+const bands = ['The Rolling Stones', 'The Beatles', 'Aerosmith', 'Led Zeppelin', 'The Who', 'Pink Floyd'];
 
-// Define a function to strip out articles ('a', 'an', 'the') from band names
-function strip(bandName) {
-  return bandName.replace(/^(a |an |the )/i, '').trim();
+function strip(article) {
+return article.replace(/^(a |an |the )/i, '').trim();
 }
 
-// Sort the band names in lexicographic order
 const sortedBands = bands.sort((a, b) => strip(a) > strip(b) ? 1 : -1);
 
-// Select the ul element
-const ul = document.querySelector('#bands');
+const bandList = document.querySelector('#bands');
 
-// Loop through the sorted bands and add them to the ul as li elements
-sortedBands.forEach(band => {
-  const li = document.createElement('li');
-  li.textContent = band;
-  ul.appendChild(li);
-});
+bandList.innerHTML = sortedBands.map(band => <li>${band}</li>).join('');
 
 
